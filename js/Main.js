ebd1434e953de388d9c254ef596dea7d60b778f4
gchen <genhan.chen@azgs.az.gov>
2012-04-18 16:22:43 -0700

    Create iconic map for vae project

--- js/Main.js
+++ js/Main.js
@@ -1,33 +1,48 @@
+var map;
+var wmsLayer;
 function init(){
-	var map = new L.Map("map");
+	map = new L.Map("map");
 	
-	/* Tilestream Layer example: */
-	var historicUrl = "http://opengis.azexperience.org/tiles/v2/azHistoric1880/{z}/{x}/{y}.png",
-		historicLayer = new L.TileLayer(historicUrl, {maxZoom: 10}); 
+	/* ESRI tiled service */
+	var imgLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer");
+	var boundLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer");
 	
-	/* ESRI tiled service example: */
-	var natGeoLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer");
-	
-	/* Bing maps example: 
-	var bingLayer = new L.TileLayer.Bing(<<Bing Maps API Key>>, "Road"); */
-	
-	/* WMS layer example: */
-	var wmsUrl = "http://opengis.azexperience.org/geoserver/wms",
-		wmsLayer = new L.TileLayer.WMS(wmsUrl, { 
+	/* WMS layer */
+	var wmsUrl = "http://opengis.azexperience.org/geoserver/wms";
+		wmsLayer = new L.TileLayer.WMS(wmsUrl, {
 			maxZoom: 10, 
-			layers: "vae:azhistoricmines", 
+			layers: "vae:aziconic", 
 			format: "image/png", 
 			transparent: true 
 		}); 
 	
-	/* WFS GeoJSON layer example: */
-	var wfsLayer = new L.GeoJSON.WFS("http://opengis.azexperience.org/geoserver/wfs", "vae:azhistoricmines", {
-		pointToLayer: function(latlng) { return new L.CircleMarker(latlng); },
+	/* WFS GeoJSON layer */
+	var wfsLayer = new L.GeoJSON.WFS("http://opengis.azexperience.org/geoserver/wfs", "vae:aziconic", {
+		pointToLayer: function(latlng) { return new L.CircleMarker(latlng, {
+												opacity: 1,
+												fillOpacity: 0,
+												radius: 16,
+												color: "#FFFF00",
+												weight: 5
+											}); 
+										},
 		popupObj: new JadeContent("templates/example.jade"),
-		popupOptions: { maxWidth: 530, centered: false },
+		popupOptions: { maxWidth: 1000, centered: true },
 		hoverFld: "name"
 	}); 
 	
 	var center = new L.LatLng(34.1618, -111.53332);
-	map.setView(center, 7).addLayer(natGeoLayer);
+	
+	map.setView(center, 7).addLayer(imgLayer);
+
+	setTimeout(function(){
+		map.addLayer(boundLayer);},
+		100);
+	
+	setTimeout(function(){
+		map.addLayer(wmsLayer);},
+		1000);
+	
+	map.addLayer(wfsLayer);
+
 }
