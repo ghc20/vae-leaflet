fa5ef6be8134e22376382c03a49dc9a92376b42e
gchen <genhan.chen@azgs.az.gov>
2012-04-19 17:30:32 -0700

    Change css for shadow; Change the position of popup

--- js/azgs-leaflet/Popup.Centered.js
+++ js/azgs-leaflet/Popup.Centered.js
@@ -6,19 +6,21 @@ L.Popup.Centered = L.Popup.extend({
 		options.className += "centered-popup";
 		L.Popup.prototype.initialize.call(this, options, source);
 	},
-	
+
 	_initLayout: function() {
 		L.Popup.prototype._initLayout.call(this);
 		this._container.removeChild(this._tipContainer);
 	},
-	
+
 	_updatePosition: function() {
 		var mapSize = this._map.getSize(),
-			pos = new L.Point(Math.round(mapSize.x / 2),Math.round(mapSize.y * 0.20));
+		pos = this._map.containerPointToLayerPoint(new L.Point(Math.round(mapSize.x / 2),Math.round(mapSize.y / 2)));
+	
+		this._containerHeight = this._container.clientHeight;
 		
-		this._containerTop = pos.y - this.options.offset.y;
+		this._containerTop = pos.y - Math.round(this._containerHeight / 2) - this.options.offset.y;
 		this._containerLeft = pos.x - Math.round(this._containerWidth / 2) + this.options.offset.x;
-
+		
 		this._container.style.top = this._containerTop + 'px';
 		this._container.style.left = this._containerLeft + 'px';
 	}
