8fb89ea0e7139a62c984903893b1fdd1bacabbf3
gchen <genhan.chen@azgs.az.gov>
2012-04-20 17:02:04 -0700

    Change map for celebrate

--- js/Main.js
+++ js/Main.js
@@ -3,40 +3,26 @@ function init(){
 	map = new L.Map("map");
 	
 	/* ESRI tiled service */
-	var imgLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer");
-	var boundLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer");
-	
-	/* WMS layer */
-	var wmsUrl = "http://opengis.azexperience.org/geoserver/wms";
-		wmsLayer = new L.TileLayer.WMS(wmsUrl, {
-			maxZoom: 10, 
-			layers: "vae:aziconic", 
-			format: "image/png", 
-			transparent: true 
-		}); 
+	var natGeoLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer");
 	
 	/* WFS GeoJSON layer */
-	var wfsLayer = new L.GeoJSON.WFS("http://opengis.azexperience.org/geoserver/wfs", "vae:aziconic", {
+	var wfsLayer = new L.GeoJSON.WFS("http://opengis.azexperience.org/geoserver/wfs", "vae:azcelebrates", {
 		pointToLayer: function(latlng) { 
 			return new L.Marker(latlng, { 
 				icon: new L.Icon({ 
-					iconUrl: "style/images/yellow-circle.png", 
+					iconUrl: $.browser.msie && parseFloat($.browser.version) < 9 ? "./style/images/celebrate.png" : "./style/images/celebrate.svg", 
 					iconSize: new L.Point(40, 40) 
 				}) 
 			});
 		},
 		popupObj: new JadeContent("templates/example.jade"),
 		popupOptions: { maxWidth: 1000, centered: true },
-		hoverFld: "name"
+		hoverFld: "city"
 	}); 
 	
 	var center = new L.LatLng(34.1618, -111.53332);
 	
-	map.setView(center, 7).addLayer(imgLayer);
-
-	setTimeout(function(){
-		map.addLayer(boundLayer);},
-		100);
+	map.setView(center, 7).addLayer(natGeoLayer);
 	
 	setTimeout(function(){
 		map.addLayer(wmsLayer);},
